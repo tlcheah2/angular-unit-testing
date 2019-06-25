a5025a4a71bdee32bac92192aaeaa63d3a806bd8
Tek Loon <tekloon.1991@gmail.com>
2019-06-25 21:35:57 +0800

    Added Isolated Test

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -6,11 +6,55 @@ import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatFormFieldModule, MatIconModule, MatInputModule } from '@angular/material';
 import { LoginService } from '../service/login.service';
+import { validUser, blankUser } from 'src/mocks';
+import { RouterTestingModule } from '@angular/router/testing';
 
 const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+const loginServiceSpy = jasmine.createSpyObj('LoginService', ['login']);
 
-describe('LoginComponent', () => {
+const testUserData = { id: 1, name: 'TekLoon'};
+const loginErrorMsg = 'Invalid Login';
+
+describe('Login Component Isolated Test', () => {
   let component: LoginComponent;
+
+  beforeEach(async(() => {
+    component = new LoginComponent(routerSpy, new FormBuilder(), loginServiceSpy);
+  }));
+
+  function updateForm(userEmail, userPassword) {
+    component.loginForm.controls['username'].setValue(userEmail);
+    component.loginForm.controls['password'].setValue(userPassword);
+  }
+
+  it('Component successfully created', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('component initial state', () => {
+    expect(component.submitted).toBeFalsy();
+    expect(component.loginForm).toBeDefined();
+    expect(component.loginForm.invalid).toBeTruthy();
+    expect(component.authError).toBeFalsy();
+    expect(component.authErrorMsg).toBeUndefined();
+  });
+
+  it('submitted should be true when onSubmit()', () => {
+    component.onSubmit(blankUser);
+    expect(component.submitted).toBeTruthy();
+    expect(component.authError).toBeFalsy();
+  });
+
+  it('form value should update from when u change the input', (() => {
+    updateForm(validUser.username, validUser.password);
+    expect(component.loginForm.value).toEqual(validUser);
+  }));
+
+  it('Form invalid should be true when form is invalid', (() => {
+    updateForm(blankUser.username, blankUser.password);
+    expect(component.loginForm.invalid).toBeTruthy();
+  }));
+});
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async(() => {
